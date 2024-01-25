421e1694a178d8c66057396be55e4893e6ea2387
pankaj143s <pankajsarnobat143@gmail.com>
2024-01-25 17:12:37 +0530

    Started HomeMenu

--- food-ordering-app/src/app/layout.js
+++ food-ordering-app/src/app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-6xl mx-auto p-4">{children}</main>
-        </body>
+        <main className="max-w-4xl mx-auto p-4">{children}</main>
+      </body>
     </html>
   );
 }
